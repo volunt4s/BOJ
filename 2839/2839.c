6b396f6315a6b3ed8c9aee04f86c8f363906a413
volunt4s <volunt4s@gmail.com>
2017-11-02 16:35:16 +0900

    Solve 2839

--- 2839/2839.c
+++ 2839/2839.c
@@ -10,31 +10,30 @@ int main(){
 	
 	int input; // for input value
 	
-	int solutionSet[10] = {0,}, minValue, i=0; // identifier i
+	int solutionSet[100] = {0,}, minValue, i=0, j; // identifier i, j
 
 	scanf("%d", &input);
 
 	for (x=0; x<=1333; x++){
-
-		if (isOverValue(x, y, input)){
-			break;
-		}
+		
 		for(y=0; y<=1000; y++){
 			
 			if (chkSolution(x, y, input) != -1){
-				solutionSet[i] = chkSolution(x, y, input);
-			}else{
-				if (isOverValue(x, y, input)) break;
-			}
-		}
-	}
+				solutionSet[i] = x+y;
+				i++;
 	
-	printf("solutionSet[0] is %d\n", solutionSet[0]);
+			}else if (isOverValue(x, y, input)){
+				y = 0;
+				break;
+			}	
+		}
+		if (isOverValue(x,y,input)) break;
+	}
 
 	if(solutionSet[0] != 0){
 		minValue = solutionSet[0];
-		for(i=1; i<10; i++){
-			if(minValue > solutionSet[i] && solutionSet[i] != 0) minValue = solutionSet[i];	
+		for(j=1; j<i; j++){
+			if(minValue > solutionSet[j] && solutionSet[j] != 0) minValue = solutionSet[j];	
 		}
 	}else{
 		printf("-1\n");
@@ -49,7 +48,6 @@ int main(){
 bool isOverValue(int x, int y, int input){
 	
 	if (((3*x) + (5*y)) > input){
-		printf("isOverVale is true, x = %d, y = %d, 3x+5y = %d\n", x, y, (3*x)+(5*y));
 		return true;
 	}
 	return false;
@@ -58,7 +56,6 @@ bool isOverValue(int x, int y, int input){
 int chkSolution(int x, int y, int input){
 
 	if (((3*x) + (5*y)) == input){
-		printf("has solved, x = %d, y = %d, x+y = %d \n", x, y, x+y);
 		return x+y;
 	}
 	
